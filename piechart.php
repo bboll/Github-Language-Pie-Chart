f3cf88c59554bdf51aeda1bd3067c403b566bc90
bboll <bboll@kent.edu>
2019-11-22 14:37:49 -0500

    Update description/test git

--- piechart.php
+++ piechart.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+    <meta name="description" content="Graph generator">
     <meta name="author" content="Brian Boll">
     <link rel="stylesheet" type="text/css" href="stylesheet.css">
 
